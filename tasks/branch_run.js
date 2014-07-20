b3fdff56b4f72a1ad42bf3ff1265f19c371ad4a6
William Candillon <w@28.io>
2014-07-20 15:02:03 -0700

    Use TRAVIS_BRANCH only in non pull requests
    If `TRAVIS_PULL_REQUEST` is true, `TRAVIS_BRANCH` will always be master.

--- tasks/branch_run.js
+++ tasks/branch_run.js
@@ -47,7 +47,7 @@ module.exports = function(grunt) {
       }
           
       //for travis-ci
-      if(process.env.TRAVIS_BRANCH){
+      if(process.env.TRAVIS_BRANCH && process.env.TRAVIS_PULL_REQUEST == "false"){
         branch = process.env.TRAVIS_BRANCH;
       }
       
